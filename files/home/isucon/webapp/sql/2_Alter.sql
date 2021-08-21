7e23e23e1d48251d94aa1e6663fa90e4bebdc45f
Kasumi Hanazuki <kasumi@rollingapple.net>
2021-08-21 05:07:49 +0000

    Drop unused columns

--- files/home/isucon/webapp/sql/2_Alter.sql
+++ files/home/isucon/webapp/sql/2_Alter.sql
@@ -1,2 +1,6 @@
 ALTER TABLE `isu_condition` ADD COLUMN `level` varchar(255) DEFAULT NULL;
 ALTER TABLE `isu_condition` ADD INDEX cond_uuid_level_timestamp (jia_isu_uuid, `level`, `timestamp`);
+
+ALTER TABLE `isu` DROP COLUMN `created_at`, DROP COLUMN `updated_at`;
+ALTER TABLE `isu_condition` DROP COLUMN `created_at`;
+ALTER TABLE `user` DROP COLUMN `created_at`;
