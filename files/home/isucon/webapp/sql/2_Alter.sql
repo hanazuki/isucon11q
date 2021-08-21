bcbcedf16d2575efcbf0ae50b422197adb13ee37
Kasumi Hanazuki <kasumi@rollingapple.net>
2021-08-21 05:12:49 +0000

    iminashi

--- files/home/isucon/webapp/sql/2_Alter.sql
+++ files/home/isucon/webapp/sql/2_Alter.sql
@@ -1,5 +1,5 @@
 ALTER TABLE `isu_condition` ADD COLUMN `level` varchar(255) DEFAULT NULL;
-ALTER TABLE `isu_condition` ADD INDEX cond_uuid_level_timestamp (jia_isu_uuid, `level`, `timestamp`);
+-- ALTER TABLE `isu_condition` ADD INDEX cond_uuid_level_timestamp (jia_isu_uuid, `level`, `timestamp`);
 
 ALTER TABLE `isu` DROP COLUMN `created_at`, DROP COLUMN `updated_at`;
 ALTER TABLE `isu_condition` DROP COLUMN `created_at`;
