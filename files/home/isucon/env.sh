e9c2f89dfbf336d571b383473b469ac33a68b260
Kasumi Hanazuki <kasumi@rollingapple.net>
2021-08-21 03:43:25 +0000

    bunsan

---
+++ files/home/isucon/env.sh
@@ -0,0 +1,6 @@
+MYSQL_HOST=isucondition-3.t.isucon.dev
+MYSQL_PORT=3306
+MYSQL_USER=isucon
+MYSQL_DBNAME=isucondition
+MYSQL_PASS=isucon
+POST_ISUCONDITION_TARGET_BASE_URL="https://isucondition-2.t.isucon.dev"
